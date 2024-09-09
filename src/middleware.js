6d234b9ac8016606940a27b3e9bdee7e8c691e2f
mohit0224 <mohitdheer24+git@gmail.com>
2024-09-09 17:21:03 +0530

    add middleware

---
+++ src/middleware.js
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+
+export const middleware = (req) => {
+	const token = req.cookies.get("token")?.value;
+	const url = req.nextUrl;
+
+	if (
+		token &&
+		(url.pathname.startsWith("/login") || url.pathname.startsWith("/register"))
+	) {
+		return NextResponse.redirect(new URL("/", req.url));
+	}
+
+	if (!token && (url.pathname === "/" || url.pathname.startsWith("/profile"))) {
+		return NextResponse.redirect(new URL("/login", req.url));
+	}
+
+	return NextResponse.next();
+};
+
+// See "Matching Paths"
+export const config = {
+	matcher: ["/", "/login", "/signup", "/profile"],
+};
