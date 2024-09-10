4e9160b399d4e7de6fd87a68dfdb9d81c4d7b46a
mohit0224 <mohitdheer24+git@gmail.com>
2024-09-10 13:43:31 +0530

    vercel

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URI}/api/v1`,
+	baseURL: `/api/v1`,
 	withCredentials: true,
 	headers: { "Content-Type": "application/json" },
 	// timeout: 10000,
