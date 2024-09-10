ba5a7eb5add3970dea4a57c5742f57145b779038
mohit0224 <mohitdheer24+git@gmail.com>
2024-09-10 14:55:01 +0530

    update axios instance

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+const isProduction = process.env.NODE_ENV === "production";
 
 const axiosInstance = axios.create({
-	baseURL: `/api/v1`,
+	baseURL: `${
+		!isProduction ? `${process.env.NEXT_PUBLIC_BACKEND_URI}/api/v1` : `/api/v1`
+	}`,
 	withCredentials: true,
 	headers: { "Content-Type": "application/json" },
 	// timeout: 10000,
